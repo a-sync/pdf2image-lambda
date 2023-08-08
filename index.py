a8330f84137f8f8396dc3ac4f3dc15ae461376ea
A.K <andras@sexybuddha.hu>
2023-08-08 14:31:30 +0200

    handle putObject events

--- index.py
+++ index.py
@@ -13,14 +13,19 @@ def pdf_to_image(event, context):
     :param context: Lambda context object.
     :return:
     """
-    if not event.key.endswith(_SUPPORTED_FILE_EXTENSION):
+
+    s3_record = event['Records'][0]['s3']
+    bucket_name = s3_record['bucket']['name']
+    object_key = s3_record['object']['key']
+
+    if not object_key.endswith(_SUPPORTED_FILE_EXTENSION):
         raise Exception(f"Only .pdf files are supported by this module.")
 
-    logging.info(f"Fetching item (bucket: '{event.bucket}', key: '{event.key}') from S3.")
+    logging.info(f"Fetching item (bucket: '{bucket_name}', key: '{object_key}') from S3.")
 
     # Fetch the image bytes
     s3 = boto3.resource('s3')
-    obj = s3.Object(event.bucket, event.key)
+    obj = s3.Object(bucket_name, object_key)
     infile = obj.get()['Body'].read()
     logging.info("Successfully retrieved S3 object.")
 
@@ -29,14 +34,14 @@ def pdf_to_image(event, context):
     images = convert_from_bytes(infile, dpi=DPI, fmt=FMT, poppler_path=poppler_path)
     logging.info("Successfully converted pdf to image.")
 
-    base_filename = event.key.rsplit('.', 1)[0]
+    base_filename = object_key.rsplit('.', 1)[0]
     for page_num, image in enumerate(images):
-        location = f"{base_filename}-new-{page_num+1}.{FMT}"
-        logging.info(f"Saving page number {str(page_num)} to S3 at location: {event.bucket}, {location}.")
+        location = f"{base_filename}-{page_num+1}.{FMT}"
+        logging.info(f"Saving page number {str(page_num)} to S3 at location: {bucket_name}, {location}.")
 
         buffer = BytesIO()
         image.save(buffer, FMT.upper())
         buffer.seek(0)
-        s3.Object(event.bucket, location).put(Body=buffer)
+        s3.Object(bucket_name, location).put(Body=buffer)
 
-    return f"PDF document ({event.key}) successfully converted to a series of images."
+    return f"PDF document ({object_key}) successfully converted to a series of images."
